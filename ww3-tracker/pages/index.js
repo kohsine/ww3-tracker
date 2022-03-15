160b22b2cf146f1b76cae14529f491e340f613f2
Kevin Oh <kevoh1516@gmail.com>
2022-03-14 20:55:10 -0400

    fix my rebase

--- ww3-tracker/pages/index.js
+++ ww3-tracker/pages/index.js
@@ -1,30 +1,5 @@
 import useSWR from 'swr'
 import Login from './auth/login'
-<<<<<<< HEAD
-import { gql_fetcher } from '../utils/fetcher'
-import Map from './components/map/map'
-
-export default function Index() {
-    const { error, data } = useSWR('{ users { nodes { username } } }', gql_fetcher);
-    if (error) return <div>Failed to load</div>
-    if (!data) return <div>Loading...</div>
-    console.log("data " + JSON.stringify(data));
-    const users = data.users.nodes;
-
-    return (
-        <Map />
-    )
-    return (
-        <div>
-            {
-                !users && <Login />
-            }
-            {users.map((user, i) => (
-                <div key={i}>{user.username}</div>
-            ))}
-        </div>
-    )
-=======
 import { fetcher } from '../utils/fetcher'
 import { Box, Container, TextField, Stack, Button } from '@mui/material'
 import { useRouter } from 'next/router'
@@ -46,5 +21,4 @@ export default function Index() {
       <span>Logged in as {username}</span> 
     </div>
   )
->>>>>>> 052eb3d (very minimal login using cookies, no auth yet)
 }
