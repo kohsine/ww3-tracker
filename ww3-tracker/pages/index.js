057f09176e32a1c4e154d0805499ed19cf616cb9
iangu48 <ian.gu@mail.utoronto.ca>
2022-03-15 00:35:01 -0400

    set up home page

--- ww3-tracker/pages/index.js
+++ ww3-tracker/pages/index.js
@@ -5,20 +5,73 @@ import { Box, Container, TextField, Stack, Button } from '@mui/material'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from "react";
 import { getCookies, getCookie, setCookies, removeCookies } from 'cookies-next';
+import SubmitContent from './components/content/submitContent'
 
 export default function Index() {
-  const [username, setUsername] = useState("");
-  
-  useEffect(() => setUsername(getCookie("username")), []);
+    const [username, setUsername] = useState(null);
+    const [createIsOpen, setCreateIsOpen] = useState(false);
 
-  const router = useRouter();
+    useEffect(() => setUsername(getCookie("username")), []);
 
-  return (
-    <div>
-      <Button size="small" id="gotoLogin" variant="text" onClick={() => {
-        router.push("/auth/login");
-      }}>Login</Button>
-      <span>Logged in as {username}</span> 
-    </div>
-  )
+    const router = useRouter();
+
+    return (
+        <div>
+            <Box height="90vh" flexDirection="column" justifyContent="flex-start" alignItems="center" display="flex" style={{ borderWidth: "1px", borderStyle: 'solid' }}>
+                {
+                    username ? <>
+
+                        <Container maxWidth="lg" >
+                            <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
+
+                                <Stack spacing={2} direction="row" justifyContent="space-between" alignItems="center" marginBottom="20px" style={{ width: '100%' }} >
+                                    <div></div>
+                                    <h1 style={{ fontWeight: 200 }}>World War III Tracker</h1>
+                                    <Stack direction="row" alignItems="baseline" spacing={4}>
+                                        <h3 style={{ fontWeight: 200 }}>Hello, {username}</h3>
+                                        <Button size="small" id="signout" variant="outlined"
+                                            onClick={() => {
+                                                removeCookies();
+                                                router.push('/');
+                                            }}
+                                        >
+                                            Signout
+                                        </Button>
+                                    </Stack>
+                                </Stack>
+
+                                <Stack direction="row" justifyContent="center" alignItems="center" spacing={2}>
+                                    <Button size="small" id="create" variant="outlined"
+                                        onClick={() => {
+                                            setCreateIsOpen(true);
+                                        }
+                                        }
+                                    >
+                                        Create New Post
+                                    </Button>
+                                </Stack>
+                                <SubmitContent open={createIsOpen} onClose={() => { setCreateIsOpen(false) }} />
+
+
+
+                            </Stack>
+
+
+
+                        </Container>
+
+
+
+                    </>
+                        : <>
+                            <h1 style={{ fontWeight: 200 }}>World War III Tracker</h1>
+
+                            <Button size="small" id="gotoLogin" variant="outlined" onClick={() => {
+                                router.push("/auth/login");
+                            }}>Login</Button>
+                        </>
+                }
+            </Box>
+        </div>
+    )
 }
