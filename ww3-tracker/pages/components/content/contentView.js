82351513e4ff45427f6e5b6fc40a76e764c454aa
iangu48 <ian.gu@mail.utoronto.ca>
2022-04-01 15:35:04 -0400

    add loader to previews

--- ww3-tracker/pages/components/content/contentView.js
+++ ww3-tracker/pages/components/content/contentView.js
@@ -1,4 +1,4 @@
-import { Card, CardActions, CardContent, CardMedia, Typography, IconButton, ButtonGroup, Stack } from '@mui/material'
+import { Card, CardActions, CardContent, CardMedia, Typography, IconButton, ButtonGroup, Stack, CircularProgress } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { gql, useQuery } from '@apollo/client'
 import { BiUpvote, BiDownvote } from 'react-icons/bi'
@@ -6,6 +6,7 @@ import { BiUpvote, BiDownvote } from 'react-icons/bi'
 
 export default function ContentView(props) {
     const [preview, setPreview] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     const QUERY = gql`
             query($url: String!) {
@@ -24,20 +25,36 @@ export default function ContentView(props) {
     useEffect(() => {
         if (data) {
             setPreview(data.preview);
+            setIsLoading(false);
         }
     }, [data]);
 
+    useEffect(() => {
+        setIsLoading(true);
+    }, [props.post]);
+
     return (
-        <Card style={{ height: '100%', margin: '5px', width: '65vh'}}>
-            <CardMedia
-                component={'img'}
-                alt={'No preview available'}
-                height="50%"
-                src={preview.images?.length > 0 ? preview.images[0] : preview.url}
-                onClick={() => {
-                    window.open(props.post.url, '_blank');
-                }}
-            />
+        <Card style={{ height: '100%', margin: '5px', width: '65vh' }}>
+            {
+                isLoading ?
+
+                    <CardContent>
+                        <Stack alignItems="center" justifyContent="center">
+                            <CircularProgress />
+                        </Stack>
+                    </CardContent>
+                    :
+
+                    <CardMedia
+                        component={'img'}
+                        alt={'No preview available'}
+                        height="50%"
+                        src={preview.images?.length > 0 ? preview.images[0] : preview.url}
+                        onClick={() => {
+                            window.open(props.post.url, '_blank');
+                        }}
+                    />
+            }
             <CardContent>
                 <Stack direction={'row'} alignItems="center">
                     <ButtonGroup variant="outlined" orientation="vertical">
